039bbcb9998995150bea13f249d022e4e6fd67bb
JoenatanSusanto <joenathansusantosusanto@gmail.com>
2024-11-26 10:48:35 +0700

    Pesan commit pertama

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,5 @@
+@vite('resources/css/out.css')
+
 <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-800">
     <div class="text-center">
         <h1 class="text-4xl font-bold text-gray-900 dark:text-gray-100 mb-6">
